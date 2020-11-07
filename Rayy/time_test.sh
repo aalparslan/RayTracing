0787ae3defa3535216ba0009bb20040ae5dfaf5b
Fatih Eser <afeserr@gmail.com>
2020-11-07 16:38:51 +0300

    Update time_test

--- Rayy/time_test.sh
+++ Rayy/time_test.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
 
+# Time test and correction test
 
-commit_hash=53ef76c
-
-rm examples/*.txt
+mkdir -p test_results/examples
+mkdir -p jpg_output
+rm *.ppm
 
 for file in examples/*
 do
     echo "Testing" $file
-    /usr/bin/time -o "$file"_"$commit_hash"_performance.txt -f "%U" ./raytracer $file  &
+    /usr/bin/time -o test_results/"$file"_performance.txt -f "%U" ./raytracer $file
+    echo $file >> test_results/"$file"_performance.txt
 done
 
-wait
 
+# Check ppm files and convert original files to jpg
 for image_file in *.ppm
 do
-    echo "Converting" $image_file
-    convert $image_file $image_file.jpg
+    echo "Converting" $image_file "to JPG"
+    python3 difference.py $image_file
+    mv difference_image.jpg test_results/$image_file.jpg
+    convert $image_file jpg_output/$image_file.jpg
 done
 
 
-
-# Writing to file...
-printf "a = {" > performance_$commit_hash.txt
-for file in examples/*.txt
-do
-    printf "'$file': '$(cat $file)', " >> performance_$commit_hash.txt
-done
-printf "}" > performance_$commit_hash.txt
-echo ""  > performance_$commit_hash.txt
 
