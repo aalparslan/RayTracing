e2b1b73da572426ea57a7abab7022aed057c4225
Fatih Eser <afeserr@gmail.com>
2020-11-07 14:27:40 +0300

    Update difference script

--- Rayy/difference.py
+++ Rayy/difference.py
@@ -1,33 +1,40 @@
 from cv2 import imread, imwrite
 import os
+import sys
+
+if len(sys.argv) == 1 or len(sys.argv) > 3:
+    print('Use:')
+    print('\tpython3 difference.py image_1 # difference of image with the copy in ../ODTUCLASS/Sample Scenes/Outputs/')
+    print('\tpython3 difference.py image_1 image_2 # difference of two images')
+
+    print()
+    print('Output is difference_image.jpg')
+
+if len(sys.argv) == 2:
+
+    im_name = sys.argv[1]
+
+    img_org = imread('../ODTUCLASS/Sample Scenes/Outputs/'+im_name)
+    img_pre = imread(im_name)
 
 
-im_name = 'horse_and_mug.ppm'
+    diff = abs(img_org - img_pre)
 
-img_org = imread('../ODTUCLASS/Sample Scenes/Outputs/'+im_name)
-img_pre = imread(im_name)
+    imwrite('difference_image.jpg', diff)
 
+if len(sys.argv) == 3:
 
-diff = abs(img_org - img_pre)
+    im_name1 = sys.argv[1]
+    im_name2 = sys.argv[2]
 
-imwrite('difference_' + im_name.split('.')[0] + '_image.jpg', diff)
-
-
-
-
-img_org = imread('bunny_float.ppm')
-img_pre = imread('bunny_double.ppm')
+    img_org = imread(im_name1)
+    img_pre = imread(im_name2)
 
 
-diff = abs(img_org - img_pre)
+    diff = abs(img_org - img_pre)
 
-imwrite('difference_bunny_float_double_image.jpg', diff)
-
-
-img_org = imread('bunny.ppm')
-img_pre = imread('bunny_double.ppm')
+    imwrite('difference_image.jpg', diff)
 
 
-diff = abs(img_org - img_pre)
-
-imwrite('difference_bunny_float_double_image2.jpg', diff)
+print()
+print('Output is difference_image.jpg')
