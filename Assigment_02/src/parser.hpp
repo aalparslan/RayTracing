1725dfce23c7481df654f766dde4a58205286774
Fatih Eser <e2166387@ceng.metu.edu.tr>
2020-12-05 18:32:21 +0300

    Update parser

--- Assigment_02/src/parser.hpp
+++ Assigment_02/src/parser.hpp
@@ -1,5 +1,5 @@
-#ifndef __HW1__PARSER__
-#define __HW1__PARSER__
+#ifndef __HW2__PARSER__
+#define __HW2__PARSER__
 
 #include <string>
 #include <vector>
@@ -8,21 +8,26 @@ namespace parser
 {
     //Notice that all the structures are as simple as possible
     //so that you are not enforced to adopt any style or design.
+    struct Vec2f
+    {
+        float x, y;
+    };
+
     struct Vec3f
     {
         float x, y, z;
     };
-    
+
     struct Vec3i
     {
         int x, y, z;
     };
-    
+
     struct Vec4f
     {
         float x, y, z, w;
     };
-    
+
     struct Camera
     {
         Vec3f position;
@@ -33,13 +38,13 @@ namespace parser
         int image_width, image_height;
         std::string image_name;
     };
-    
+
     struct PointLight
     {
         Vec3f position;
         Vec3f intensity;
     };
-    
+
     struct Material
     {
         Vec3f ambient;
@@ -61,26 +66,45 @@ namespace parser
         int v1_id;
         int v2_id;
     };
-    
+
     struct Mesh
     {
+        int texture_id;
         int material_id;
         std::vector<Face> faces;
+        std::string transformations;
     };
-    
+
     struct Triangle
     {
+        int texture_id;
         int material_id;
         Face indices;
+        std::string transformations;
     };
-    
+
     struct Sphere
     {
+        int texture_id;
         int material_id;
         int center_vertex_id;
         float radius;
+        std::string transformations;
     };
-    
+
+    struct Rotation
+    {
+        float angle, x, y, z;
+    };
+
+    struct Texture
+    {
+        std::string imageName;
+        std::string interpolation;
+        std::string decalMode;
+        std::string appearance;
+    };
+
     struct Scene
     {
         //Data
@@ -92,10 +116,15 @@ namespace parser
         std::vector<PointLight> point_lights;
         std::vector<Material> materials;
         std::vector<Vec3f> vertex_data;
+        std::vector<Vec2f> tex_coord_data;
         std::vector<Mesh> meshes;
         std::vector<Triangle> triangles;
         std::vector<Sphere> spheres;
-        
+        std::vector<Vec3f> translations;
+        std::vector<Vec3f> scalings;
+        std::vector<Rotation> rotations;
+        std::vector<Texture> textures;
+
         //Functions
         void loadFromXml(const std::string& filepath);
     };
