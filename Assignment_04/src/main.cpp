e7501d9cbf02711f86d239aace300e49f85fbdec
Fatih Eser <afeserr@gmail.com>
2021-01-13 02:20:12 -0800

    bir seyler yazdim

--- Assignment_04/src/main.cpp
+++ Assignment_04/src/main.cpp
@@ -8,7 +8,6 @@
 
 using namespace std;
 static GLFWwindow* win = NULL;
-int widthWindow = 1000, heightWindow = 1000;
 
 // Shaders
 GLuint idProgramShader;
@@ -22,21 +21,18 @@ GLuint idMVPMatrix;
 GLuint idVertexBuffer;
 GLuint idIndexBuffer;
 
-int textureWidth, textureHeight;
-float heightFactor;
-
 
 
 //Camera initialCamera;
 struct Camera {
-    glm::vec3 posiotion;
+    glm::vec3 position;
     glm::vec3 gaze;
     glm::vec3 up;
     glm::vec3 left;
-    
+
 };
 
-Camera camera;
+
 glm::vec3 lightPos;
 GLuint depthMapFBO;
 GLuint depthCubemap;
@@ -53,7 +49,7 @@ struct Vertex {
     glm::vec3 vPosition;
     // texture pos
     glm::vec2 tPosition;
-    
+
 };
 
 struct Index {
@@ -74,65 +70,92 @@ static void keyCallback(GLFWwindow* window, int key, int scancode, int action, i
         glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
-void initVerticesAndIndices(){
-    
+void initVerticesAndIndices(int textureWidth, int textureHeight, vector<Index> &indices, vector<Vertex> &vertices){
+
     // initialize indices
     int k = 0;
     for(int i = 0; i < textureHeight; i++){
         for(int j = 0; j < textureWidth; j++){
-            
+
             // triangle1 in pixel
             // left upperside, left lowerside,  right upperside
             glm::vec3 indice1 = glm::vec3(i*textureWidth + j,(i+1)*textureWidth + j,i*textureWidth + j + 1);
             //triangle2
             //left lowerside, right lowerside, right upperside
             glm::vec3 indice2 = glm::vec3((i+1)*textureWidth + j,(i+1)*textureWidth + j+1,i*textureWidth + j + 1);
-        
+
             indices[k].pos = indice1;
             k++;
             indices[k].pos = indice2;
             k++;
-            
-            
+
+
             // initialize vertices
             // x,y,z
             glm::vec3 vPosition = glm::vec3(j,0.0,i);
             // u,v
             glm::vec2 tPosition = glm::vec2(1 - ((float) j)/textureWidth , 1 - ((float) i)/textureHeight);
-            
+
             vertices[i*textureWidth + j].vPosition = vPosition;
             vertices[i*textureWidth + j].tPosition = tPosition;
         }
     }
 }
 
-void initCamera(){
-    //The camera will be positioned initially at (w/2, w/10, -w/4) where w is the width of the texture image
-    camera.posiotion = glm::vec3(textureWidth/2.0, textureWidth/ 10.0, -textureWidth/ 4.0);
-    camera.gaze = glm::vec3(0.0, 0.0, 1.0);
-    camera.up = glm::vec3(0.0, 1.0, 0.0);
-    camera.left = glm::vec3(glm::normalize(glm::cross(camera.gaze, camera.up)));
-    
+void initCamera(int textureWidth, int textureHeight, Camera &camera){
+    // The camera will be positioned initially at (w/2, w/10, -w/4) where w is the width of the texture image
+    camera.position = glm::vec3(textureWidth / 2.0, textureWidth / 10.0, - textureWidth / 4.0);
+    // Initial gaze
+    camera.gaze     = glm::vec3(0.0, 0.0, 1.0);
+    camera.up       = glm::vec3(0.0, 1.0, 0.0);
+
     // set up mvp...
-    
-    
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(
+      // Eye position
+      camera.position.x,
+      camera.position.y,
+      camera.position.z,
+      // Reference point position
+      camera.gaze.x,
+      camera.gaze.y,
+      camera.gaze.z,
+      // Up vector
+      camera.up.x,
+      camera.up.y,
+      camera.up.z
+    );
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(
+      60.,
+      (float) textureWidth / textureHeight,
+      1,
+      1000
+    );
+    glMatrixMode(GL_MODELVIEW);
+
+
+
+
 }
 
-void initialize(){
-    
+void initialize(Camera &camera, int textureWidth, int textureHeight, vector<Index> &indices, vector<Vertex> &vertices){
+
     // initizalize shaders with idProgramShader
-    string vertexShader = "shader.vert";
-    string fragmentShader = "shader.frag";
+    string vertexShader = "src/shaders/shader.vert";
+    string fragmentShader = "src/shaders/shader.frag";
     initShaders(idProgramShader, vertexShader , fragmentShader );
-    
-    
+
+
     //create a VAO
     GLuint VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
-    
-    initVerticesAndIndices();
-    
+
+    initVerticesAndIndices(textureWidth, textureHeight, indices, vertices);
+
     //Create vbo for vertices
     glGenBuffers(1, &idVertexBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, idVertexBuffer);
@@ -141,78 +164,86 @@ void initialize(){
     glGenBuffers(1, &idIndexBuffer);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idIndexBuffer);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Index)*indices.size(), &indices[0], GL_STATIC_DRAW);
-    
+
     glVertexAttribPointer(0, 1, GL_V3F, GL_FALSE, sizeof(glm::vec3) + sizeof(glm::vec2),(void *) 0);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 1, GL_V2F, GL_FALSE, sizeof(glm::vec3) + sizeof(glm::vec2), (void *) (3 * sizeof(glm::vec3)));
     glEnableVertexAttribArray(1);
     glUseProgram(idProgramShader);
-    initCamera();
-    
-    
-    
-    
-    
-    
+    initCamera(textureWidth, textureHeight, camera);
+
+
+
+
+
+
 }
 
 int main(int argc, char *argv[]) {
-    
+
+    Camera camera;
+    int textureWidth, textureHeight;
+    float heightFactor;
+    int widthWindow = 1000, heightWindow = 1000;
+
+
+
     if (argc != 3) {
         printf("Please provide height and texture image files!\n");
         exit(-1);
     }
-    
+
     glfwSetErrorCallback(errorCallback);
-    
+
     if (!glfwInit()) {
         exit(-1);
     }
-    
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    
+
    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_ANY_PROFILE); // This is required for remote
      //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_ANY_PROFILE); // This might be used for local
-    
-    
+
+
     /// TRADITIONAL  MacOS FLAGS TRY TO REMOVE THEM LATER ON...
     glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     /////////////////////////////////////////////////////////
-    
+
     win = glfwCreateWindow(widthWindow, heightWindow, "CENG477 - HW4", NULL, NULL);
-    
+
     if (!win) {
         glfwTerminate();
         exit(-1);
     }
     glfwMakeContextCurrent(win);
-    
+
     GLenum err = glewInit();
     if (err != GLEW_OK) {
         fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
-        
+
         glfwTerminate();
         exit(-1);
     }
-    
-    
-    initialize();
-    
- 
-    
-    glfwSetKeyCallback(win, keyCallback);
-    
+
     initTexture(argv[1], argv[2], &textureWidth, &textureHeight);
-    
-    
+
+    initialize(camera, textureWidth, textureHeight, indices, vertices);
+
+
+
+    glfwSetKeyCallback(win, keyCallback);
+
+
+
+
     while(!glfwWindowShouldClose(win)) {
         glfwSwapBuffers(win);
         glfwPollEvents();
     }
-    
-    
+
+
     glfwDestroyWindow(win);
     glfwTerminate();
     return 0;
