2056f23807ca69d099054c55c8e6caf0d7fa2d9e
aalparslan <33001818+aalparslan@users.noreply.github.com>
2021-01-30 21:12:48 +0300

    Update main.cpp

--- Assignment_04/src/main.cpp
+++ Assignment_04/src/main.cpp
@@ -4,7 +4,7 @@
 #include "glm/gtx/rotate_vector.hpp"
 #include "glm/gtc/type_ptr.hpp"
 #include <vector>
-#include "helper.hpp"
+#include "helper.h"
 
 using namespace std;
 
@@ -76,7 +76,7 @@ glm::vec3 lightPosition;
 int textureOffset = 0;
 
 // locations
-int locTextureHeight, locTextureWidth, locTexture, locMVP, locHeightFactor, locCameraPosition, locLightPosition, locTextureOffset;
+int locTextureHeight, locTextureWidth, locTexture, locMVP, locHeightFactor, locCameraPosition, locLightPosition, locTextureOffset, locHeightMap;
 
 // Hold if currently full screen or not
 bool fullScreenMode = false;
@@ -90,22 +90,22 @@ int windowModeXStart, windowModeYStart, windowModeXSize, windowModeYSize;
 void initGeometry(float &heightFactor, float &aspectRatio, float &nearPlane, float &farPlane,  float &YfieldOfView){
     // The camera will be positioned initially at (w/2, w/10, -w/4) where w is the width of the texture image
     initialCamera.position = glm::vec3(widthTexture / 2.0, widthTexture / 10.0, - widthTexture / 4.0);
-    initialCamera.gaze = glm::vec3(0.0, 0.0, 0.1);
+    initialCamera.gaze = glm::vec3(0.0, 0.0, 1.0);
     initialCamera.up = glm::vec3(0.0, 1.0, 0.0);
-
+    
     camera = initialCamera;
     //init mvp
     Model = glm::mat4(1.0);
     View = glm::lookAt(camera.position, camera.position + camera.gaze, camera.up);
     Projection = glm::perspective(YfieldOfView, aspectRatio, nearPlane, farPlane);
     MVP = Projection * View * Model;
-
+    
     // initialize light as specified in the pdf.
     lightPosition = glm::vec3(widthTexture/ 2.0, 100, heightTexture / 2.0);
     // Initial gaze - RADIANS!!!!!
     camera.pitchAngle = 0.;
     camera.yawAngle   = 0.;
-
+    
     // Initial speed
     camera.speed = 0;
     heightFactor = 10;
@@ -120,7 +120,7 @@ static void keyCallback(GLFWwindow* window, int key, int scancode, int action, i
      * Alttaki yerde sirayla bakiyoruz...
      * OPENGL sebebiyle kamerayi local yapamiyoruz, global olmak zorunda :/
      */
-
+    
     if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
         glfwSetWindowShouldClose(window, GLFW_TRUE);
     }else if(key == GLFW_KEY_A  && (action == GLFW_PRESS || action == GLFW_REPEAT)){
@@ -150,18 +150,18 @@ static void keyCallback(GLFWwindow* window, int key, int scancode, int action, i
             // Do not forget to store the current view!
             glfwGetWindowPos(window, &windowModeXStart, &windowModeYStart);
             glfwGetWindowSize(window, &windowModeXSize, &windowModeYSize);
-
+            
             fullScreenMode = true;
-
+            
             // Get the video mode
             const GLFWvidmode* videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
+            
             // Set the window to the expected mode!
             glfwSetWindowMonitor(window, glfwGetPrimaryMonitor(), 0, 0, videoMode->width, videoMode->height, 0);
         }else{
             // Set windowed operation flag
             fullScreenMode = false;
-
+            
             // Set back!
             glfwSetWindowMonitor(window, NULL, windowModeXStart, windowModeYStart, windowModeXSize, windowModeYSize, 0);
         }
@@ -191,24 +191,24 @@ static void keyCallback(GLFWwindow* window, int key, int scancode, int action, i
 void initVerticesAndIndices(    vector<int> &indices,
                             vector<Vertex> &vertices){
     // initiate indices
-
+    
     for(int i = 1; i < heightTexture+1; i++){
         for(int j = 1; j < widthTexture+1; j++){
-
+            
             // asagidaki if en sagdaki vertexlerle en soldaki arasinda ucgen olusturmayip diger durumlarda olusturuyor.
             if(((i)*widthTexture + j + 1) % (widthTexture +1) != 0){
-
+                
                 indices.push_back((i-1)*widthTexture + j-1);
                 indices.push_back((i)*widthTexture + j );
                 indices.push_back((i-1)*widthTexture + j);
-
+                
                 indices.push_back((i-1)*widthTexture + j );
                 indices.push_back((i)*widthTexture + j );
                 indices.push_back((i)*widthTexture + j + 1);
             }
         }
     }
-
+    
     //  initiate vertices
     for(int i = 0; i < heightTexture+1; i++){
         for(int j = 0; j < widthTexture+1; j++){
@@ -223,7 +223,7 @@ void initVerticesAndIndices(    vector<int> &indices,
 
 // uniforms are initialized below for shaders' usage
 void initializeUniforms( float &heightFactor){
-
+    
     locMVP = glGetUniformLocation(idProgramShader, "MVP");
     glUniformMatrix4fv(locMVP, 1, GL_FALSE, glm::value_ptr(MVP));
     locHeightFactor = glGetUniformLocation(idProgramShader,"heightFactor");
@@ -240,113 +240,115 @@ void initializeUniforms( float &heightFactor){
     glUniform1i(locTexture, 1);
     locTextureOffset = glGetUniformLocation(idProgramShader, "textureOffset");
     glUniform1i(locTextureOffset, textureOffset);
-
+    
+    locHeightMap = glGetUniformLocation(idProgramShader, "heightMapTexture");
+    glUniform1i(locHeightMap, 0);
 }
 
 
 
 void initializeOPENGL(int argc, char *argv[]){
-
+    
     // Default ones...
     if (argc != 3) {
         printf("Please provide height and texture image files!\n");
         exit(-1);
     }
-
+    
     glfwSetErrorCallback(errorCallback);
-
+    
     if (!glfwInit()) {
         exit(-1);
     }
-
+    
     glewExperimental = GL_TRUE;
     glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
-
+    
     win = glfwCreateWindow(widthWindow, heightWindow, "CENG477 - HW4", NULL, NULL);
-
+    
     if (!win) {
         glfwTerminate();
         exit(-1);
     }
-
+    
     // Keyboard interrupts
     glfwSetKeyCallback(win, keyCallback);
     glfwMakeContextCurrent(win);
     printf("OpenGL version: %s - %s\n",glGetString(GL_RENDERER),glGetString(GL_VERSION));
-
+    
     GLenum err = glewInit();
     if (err != GLEW_OK) {
         fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
-
+        
         glfwTerminate();
         exit(-1);
     }
     glfwSwapInterval(1);
-
+    
 }
 
 int main(int argc, char *argv[]) {
-
+    
     ////////////////////
     GLuint vertex_buffer;
     GLuint normal_buffer;
     GLuint tcoord_buffer;
     GLuint index_buffer;
-
+    
     vector<GLfloat> vdata;
     vector<GLfloat> tdata;
     vector<GLfloat> ndata;
     vector<GLuint> idata;
     ///////////////////////
-
+    
     // containers for data
     vector<int> indices;
     vector<Vertex> vertices;
-
+    
     // Initialize the opengl framework
     initializeOPENGL(argc, argv);
     //////////////////////////
     initTexture(argv[1], argv[2], &widthTexture, &heightTexture);
     initVerticesAndIndices( indices, vertices);
-
+    
     glGenBuffers(1, &vertex_buffer);
     glGenBuffers(1, &normal_buffer);
     glGenBuffers(1, &tcoord_buffer);
     glGenBuffers(1, &index_buffer);
-
+    
     int totalnumberOfVertices = vertices.size();
-
+    
     for(vector<Vertex>::iterator it = vertices.begin(); it != vertices.end(); ++it){
         ndata.push_back(it->normal.x);
         ndata.push_back(it->normal.y);
         ndata.push_back(it->normal.z);
-
+        
         vdata.push_back(it->vPosition.x);
         vdata.push_back(it->vPosition.y);
         vdata.push_back(it->vPosition.z);
-
+        
         tdata.push_back(it->textureCoordinate.x);
         tdata.push_back(it->textureCoordinate.y);
     }
     for(vector<int>::iterator it = indices.begin(); it != indices.end(); ++it){
         idata.push_back(*it);
     }
-
+    
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*3*totalnumberOfVertices, vdata.data(), GL_STATIC_DRAW);
-
+    
     glBindBuffer(GL_ARRAY_BUFFER, normal_buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*3*totalnumberOfVertices, ndata.data(), GL_STATIC_DRAW);
-
+    
     glBindBuffer(GL_ARRAY_BUFFER, tcoord_buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*2*totalnumberOfVertices, tdata.data(), GL_STATIC_DRAW);
-
+    
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint)*indices.size(), idata.data(), GL_STATIC_DRAW);
-
+    
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
+    
     //  Initialize the shaders
     string vertexShader = "src/shaders/shader.vert";
     string fragmentShader = "src/shaders/shader.frag";
@@ -359,26 +361,25 @@ int main(int argc, char *argv[]) {
     glEnable(GL_TEXTURE_2D);
     glEnable(GL_DEPTH_TEST);
     // From helper...
-
+    
     // Initialize all the remaining properties
     // initializeBuffers( indices, vertices);
     initGeometry(  heightFactor,  aspectRatio,  nearPlane,  farPlane,   YfieldOfView);
-
+    
     // initialize uniforms
     initializeUniforms( heightFactor);
-
+    
     //glViewport(0, 0, widthWindow, heightWindow);
-    glEnable(GL_DEPTH_TEST);
     while(!glfwWindowShouldClose(win)) {
         //////////
         // Automatic window size
         int currentWindowWidth;
         int currentWindowHeight;
         glfwGetFramebufferSize(win, &currentWindowWidth, &currentWindowHeight);
-
+        
         glViewport(0, 0, currentWindowWidth, currentWindowHeight);
-
-
+        
+        
         glBindBuffer(GL_ARRAY_BUFFER, normal_buffer);
         glNormalPointer(GL_FLOAT,0,0);
         glBindBuffer(GL_ARRAY_BUFFER, tcoord_buffer);
@@ -386,23 +387,23 @@ int main(int argc, char *argv[]) {
         glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
         glVertexPointer(3,GL_FLOAT,0,0);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
-
-        glUniform1i(glGetUniformLocation(idProgramShader, "idHeightTexture"), 1);
-        glActiveTexture(GL_TEXTURE0 + 1);
+        
+        glUniform1i(glGetUniformLocation(idProgramShader, "idHeightTexture"), 0);
+        glActiveTexture(GL_TEXTURE0 );
         glBindTexture(GL_TEXTURE_2D,idHeightTexture);
-
+        
         glUniform1i(glGetUniformLocation(idProgramShader, "idJpegTexture"), 1);
-        glActiveTexture(GL_TEXTURE0 + 0);
+        glActiveTexture(GL_TEXTURE1 );
         glBindTexture(GL_TEXTURE_2D,idJpegTexture);
-
+        
         glUniform1f(locHeightFactor, heightFactor);
         glUniform3fv(locLightPosition, 1, glm::value_ptr(lightPosition));
-
+        
         glUniform1f(locTextureOffset, textureOffset);
         glUniform1i(locTextureOffset, textureOffset);
         glUseProgram(idProgramShader);
-
-
+        
+        
         glClearColor(0,0,0,1);
         glClearDepth(1.0);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -414,19 +415,19 @@ int main(int argc, char *argv[]) {
                                                // z
                                                cos(camera.pitchAngle)*cos(camera.yawAngle)
                                                ));
-
+        
         // Calculate the camera position in time
         camera.position = camera.position + camera.speed * camera.gaze;
-
+        
         View = glm::lookAt(camera.position, camera.position + camera.gaze, camera.up);
         Projection = glm::perspective(YfieldOfView, aspectRatio, nearPlane, farPlane);
         MVP = Projection * View * Model;
-
+        
         // Send new geometry to OpenGL
         glUniformMatrix4fv(locMVP, 1, GL_FALSE, glm::value_ptr(MVP));
         glUniform3fv(locCameraPosition, 1, glm::value_ptr(camera.position));
         glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, nullptr);
-
+        
         glfwSwapBuffers(win);
         glfwPollEvents();
     }
